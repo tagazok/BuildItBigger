b0f10df1312fdf0dfa6a5ef8f6f4a5e4633a437f
tagazok <leplus.olivier@gmail.com>
2016-01-03 21:34:47 +0100

    Add free and paid version Manifest

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -27,7 +27,6 @@ public class MainActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
 
@@ -36,24 +35,11 @@ public class MainActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
 
     public void tellJoke(View view){
-        /*Joke j = new Joke();
-        String joke = j.getJoke();
-
-        Intent intent = new Intent(this, JokeActivity.class);
-
-        intent.putExtra(JokeActivity.JOKE_KEY, joke);
-        startActivity(intent);*/
 
         new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
     }
